033074eb8731e167a47dde00aedcdefb755182e5
liyao <liyao123747@gmail.com>
2018-04-06 15:38:07 +0800

    修改语音识别问题

--- app/src/main/java/com/example/voiceassistant/voiceassistant/MainActivity.java
+++ app/src/main/java/com/example/voiceassistant/voiceassistant/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.voiceassistant.voiceassistant.bean.DictationResult;
+import com.example.voiceassistant.voiceassistant.utils.JsonParser;
 import com.example.voiceassistant.voiceassistant.utils.RequestPermission;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -21,7 +22,19 @@ import com.iflytek.cloud.SpeechUtility;
 import com.iflytek.cloud.ui.RecognizerDialog;
 import com.iflytek.cloud.ui.RecognizerDialogListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends Activity {
 
@@ -30,6 +43,8 @@ public class MainActivity extends Activity {
     private RecognizerDialog iatDialog;
     private TextView etText;
     private RequestPermission requestPermission;
+    // 用HashMap存储听写结果
+    private HashMap<String, String> mIatResults = new LinkedHashMap<String, String>();
 
     private InitListener mInitListener = new InitListener() {
         @Override
@@ -68,28 +83,25 @@ public class MainActivity extends Activity {
 
     class MyRecognizerDialogListener implements RecognizerDialogListener {
 
-        String resultJson = "[";
-
         @Override
         public void onResult(RecognizerResult recognizerResult, boolean isLast) {
-            if (!isLast) {
-                resultJson += recognizerResult.getResultString() + ",";
-            } else {
-                resultJson += recognizerResult.getResultString() + "]";
-            }
+            String text = JsonParser.parseIatResult(recognizerResult.getResultString());
 
-            if (isLast) {
-                //解析语音识别后返回的json格式的结果
-                Gson gson = new Gson();
-                List<DictationResult> resultList = gson.fromJson(resultJson,
-                        new TypeToken<List<DictationResult>>() {
-                        }.getType());
-                String result = "";
-                for (int i = 0; i < resultList.size() - 1; i++) {
-                    result += resultList.get(i).toString();
-                }
-                etText.setText(result);
+            String sn = null;
+            // 读取json结果中的sn字段
+            try {
+                JSONObject resultJson = new JSONObject(recognizerResult.getResultString());
+                sn = resultJson.optString("sn");
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
+            mIatResults.put(sn, text);
+
+            StringBuffer resultBuffer = new StringBuffer();
+            for (String key : mIatResults.keySet()) {
+                resultBuffer.append(mIatResults.get(key));
+            }
+            etText.setText(resultBuffer.toString());
         }
 
         @Override
@@ -103,11 +115,41 @@ public class MainActivity extends Activity {
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.btn_mic:
+                    mIatResults.clear();
                     //开始听写，需将sdk中的assets文件下的文件夹拷入项目的assets文件夹下（没有的话自己新建）
                     iatDialog.show();
+
+//                    try {
+//                        URL url = new URL("http://v.juhe.cn/toutiao/index?type=top&key=26015a44325349d4daeff7b41429ce81");
+//                        requestNewsJsonData(url);
+//                    } catch (MalformedURLException e) {
+//                        e.printStackTrace();
+//                    }
                     break;
             }
         }
+    }
+
+    private String requestNewsJsonData(final URL url) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request.Builder().url(url).build();
+                Response response = null;
+                try {
+                    response = okHttpClient.newCall(request).execute();
+                    if (response.isSuccessful()) {
+                        Log.d(TAG, "run: response.code()=" + response.code());
+                        Log.d(TAG, "run: response.message()=" + response.message());
+                        Log.d(TAG, "run: response.body()=" + response.body().string());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        return null;
     }
 
 }
